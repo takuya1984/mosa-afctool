be1d4f9e5a0f8fa258ab87256d91ae1305bfd31c
daisuke-3 <d.kohgami@mosa-architect.com>
2012-09-12 01:34:41 +0900

    作成中シェル一旦リリース

---
+++ script/bin/log_conv_web.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#-----------------------------------------------------------------------
+# name   : log_conv_web.sh
+# param  : $1 - 抽出対象ログファイル
+# return : 0 - normal
+#        : 1 - error
+#
+# 処理概要 : 指定されたログファイルから電文ログ形式ファイルを生成する
+#-----------------------------------------------------------------------
+
+. $(dirname $0)/../conf/script.conf
+
+TODAY=`date +"%Y%m%d%H%M%S"`
+logfile=$1
+
+status=0    # 0:idle 1:prossecing 
+updw_flg=0  # 1:up 2:dw
+filename=""
+COM="",TIME=""
+
+while IFS= read LINE
+do
+	#--------------------------------
+	# 電文単位に抽出を行う.
+	#--------------------------------
+	if [ $status -eq 0 ];then
+		echo "${LINE}" | grep " TRACE " > /dev/null 2>&1
+		RC=$?
+		if [ $RC -ne 0 ];then
+			continue
+		else
+			status=1
+			COM="",TIME=""
+
+			if echo "${LINE}" | grep "Outgoing Request Message" > /dev/null 2>&1
+			then
+				updw_flg=1
+			else
+				updw_flg=2
+			fi
+#			echo "${LINE}" | awk '$4 ~ /受信/{print $0}'
+
+			tmpfile="${BASEDIR}/tmp/$(echo "${LINE}"| cut -b1-19 | sed -e "s/\//-/g" -e "s/ /_/g" -e "s/://g")_$(echo "${LINE}" | cut -b21-23)_${updw_flg}.dat.$$"
+			TIME=$(echo "${LINE}"| cut -b1-19 | sed -e "s/\//-/g" -e "s/ /_/g" -e "s/://g")
+		fi
+	fi
+	
+	echo "${LINE}" >> $tmpfile
+
+	#--------------------------------
+	# ヘッダーの内容チェック
+	#--------------------------------
+	if echo "${LINE}" | grep "<strComUpHeadDt>" > /dev/null 2>&1
+	then
+		COM=$(echo "${LINE}" | sed -e "s/.*<strComUpHeadDt>//" | sed -e "s/<\/strComUpHeadDt>.*//")
+		
+	fi
+
+	#--------------------------------
+	# Webサーバログかどうかの判定
+	# 情報系APor勘定系APログの場合、処理を終了
+	# (本番環境であればこの処理は該当しないはず)
+	#--------------------------------
+	if echo "${LINE}" | grep "<ns1:ResponseMessageList" > /dev/null 2>&1
+	then
+		status=0
+		rm -f $tmpfile > /dev/null 2>&1
+	fi
+
+	#--------------------------------
+	# 電文の最終処理行チェック
+	#--------------------------------
+	if echo "${LINE}" | grep "</env:Envelope>" > /dev/null 2>&1
+	then
+		status=0
+		# ファイルコピー
+		cp -p $tmpfile ${WEB_LOG_DIR}/$(echo "${TIME}")_$(echo "${COM}" | cut -b1-5)_$(echo "${COM}" | cut -b6-6)_$(echo "${COM}" | cut -b7-13)_${updw_flg}.dat
+
+		rm -f $tmpfile > /dev/null 2>&1
+		COM="",TIME=""
+	fi
+
+done < $logfile
---
+++ script/bin/log_regist_client.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#-----------------------------------------------------------------------
+# name   : log_regist_client.sh
+# param  : none
+# return : 0 - normal
+#        : 1 - error
+#
+# 処理概要 : VBログを管理DBに登録する.
+#-----------------------------------------------------------------------
+
+. $(dirname $0)/../conf/script.conf
+
+TODAY=`date +"%Y%m%d%H%M%S"`
+COM="",ERR="",ONL="",CSS="";
+
+for file in $(ls ${CLIENT_LOG_DIR})
+do
+	LOG_CD="",LOG_OUTPUT_DATE="",UPDW_FLG="",CL_CD="",OPE_CD="",DENBUN_CD="",CLIENT_SERIAL_NUMBER="",CONTINUE_DENBUN_FLG="",TRANSACTION_NUMBER=""
+	while IFS= read LINE
+	do
+
+		opecd=$(echo "${LINE}" | awk '{print $5}')
+
+		# -------------------------------
+		# 上り下りの判定
+		# -------------------------------
+		if echo "${LINE}" | grep "送信" > /dev/null 2>&1
+		then
+			UPDW_FLG="1"
+		else
+			UPDW_FLG="2"
+		fi
+
+		# -------------------------------
+		# ヘッダ情報取得
+		# -------------------------------
+		if [ "$opecd" == "Com" ];then
+			COM=$(echo "${LINE}" | sed -e "s/.* Com //")
+			LOG_OUTPUT_DATE=$(echo "${LINE}" | awk '{print $1 $2}' | sed -e "s/\///g" -e "s/://g")
+			LOG_CD="1"
+			CL_CD=$(echo "${COM}" | cut -b1-5)
+			DENBUN_CD=$(echo "${COM}" | cut -b7-13)
+			CLIENT_SERIAL_NUMBER=$(echo "${COM}" | cut -b14-17)
+			CONTINUE_DENBUN_FLG=$(echo "${COM}" | cut -b18-18)
+
+			# 下りの場合
+			if [ $UPDW_FLG == "2" ];then
+				# トランザクション処理連番
+				TRANSACTION_NUMBER=$(echo "${COM}" | cut -b23-30)
+			fi
+		fi
+
+	done < ${CLIENT_LOG_DIR}/$file
+	
+	# -------------------------------
+	# 管理DBに登録
+	# -------------------------------
+	java -jar ${JAVA_NORIN_JAR} RegistLogData "log_cd=${LOG_CD}" "log_output_date=${LOG_OUTPUT_DATE}" "up_down_cd=${UPDW_FLG}" "cl_cd=${CL_CD}" "ope_cd=${OPE_CD}" "denbun_cd=${DENBUN_CD}" "client_serial_number=${CLIENT_SERIAL_NUMBER}" "continue_denbun_flg=${CONTINUE_DENBUN_FLG}" "transaction_number=${TRANSACTION_NUMBER}" "log_data_file=${file}"
+done
