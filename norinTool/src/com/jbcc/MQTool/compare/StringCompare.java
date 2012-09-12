baf4de3d983e808bb9794e5570a7ea12c17619af
TOMO YOSHIDA <tomo.yoshida@mosa-architect.com>
2012-09-12 13:00:22 +0900

    Gitデビュー

---
+++ norinTool/src/com/jbcc/MQTool/compare/StringCompare.java
@@ -0,0 +1,74 @@
+package com.jbcc.MQTool.compare;
+
+import java.io.PrintStream;
+import java.util.List;
+
+import com.jbcc.MQTool.util.ToolsPrintStream;
+
+public class StringCompare {
+
+	// 出力用stream
+	private static PrintStream ps = new ToolsPrintStream();
+
+	/**
+	 * ２つのList oとnの現在の要素を比較する。
+	 *
+	 * @return 比較結果
+	 */
+	private static String compareNext(List<String> o, List<String> n, int i) {
+
+		StringBuilder sb = null;
+
+		String s1 = null;
+		String s2 = null;
+		if (i < o.size()) {
+			s1 = o.get(i);
+		}
+
+		if (i < n.size()) {
+			s2 = n.get(i);
+		}
+		i++;
+
+		if (s1 == null && s2 == null) {
+			return null;
+		} else if (s1 != null && s2 != null && s1.equals(s2)) {
+			sb = new StringBuilder("○");
+		} else {
+			sb = new StringBuilder("×");
+		}
+
+		sb.append("\t").append(s1).append("\t").append(s2);
+
+		return sb.toString();
+	}
+
+	/**
+	 * 出力先を変更する<br>
+	 * セットしなければ標準出力(ToolsPrintStreamによる)
+	 *
+	 * @param ps
+	 *            出力stream
+	 */
+	public static void setStream(PrintStream ps) {
+		StringCompare.ps = ps;
+	}
+
+	/**
+	 * ２つのList oとnを全量比較し、結果を出力する。<br>
+	 *
+	 * @param o
+	 *            比較元(旧)
+	 * @param n
+	 *            比較先(新)
+	 */
+	public static void compareAll(List<String> o, List<String> n) {
+
+		String s = null;
+		int i = 0;// oとnのlengthが違うかもしれないのでカウンタ
+
+		while ((s = compareNext(o, n, i++)) != null) {
+			ps.println(s);
+		}
+	}
+}
