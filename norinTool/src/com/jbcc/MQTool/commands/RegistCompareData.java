4cb73c66bfcdedaa8b3a3f8fd0fb1be7b969451c
daisuke-3 <d.kohgami@mosa-architect.com>
2012-09-19 16:51:08 +0900

    比較除外項目登録I/F追加

---
+++ norinTool/src/com/jbcc/MQTool/commands/RegistCompareData.java
@@ -0,0 +1,71 @@
+package com.jbcc.MQTool.commands;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import com.jbcc.MQTool.controller.ToolCommand;
+import com.jbcc.MQTool.controller.ToolException;
+import com.jbcc.MQTool.util.Utility;
+
+/**
+ * 比較除外項目データを登録する.
+ * IDが重複する場合はupdate IDが存在しない場合はinsertになる
+ * 
+ */
+public class RegistCompareData extends ToolCommand {
+
+	@Override
+	public void execute(String[] args) throws Exception {
+
+		// insert update なので merge文
+		String sql = RESOURCE.getSql("ResistCompareData1");
+
+		// 引数を辞書かしてSQL文作成
+		Map<String, String> datas = Utility.splitArgs(args);
+		String dataid = datas.get("id");
+		datas.remove("id");
+		String insertQuery = insertQuery(datas);
+		String updateQuery = Utility.updateQuery(datas);
+
+		// preparedstatementのパラメータ作成
+		Collection<String> values = new ArrayList<String>();
+		// 条件句のID
+		values.add(dataid);
+		// update句の値
+		values.addAll(datas.values());
+		// insert句の値
+		values.addAll(datas.values());
+
+		// SQL実行
+		int counts = RESOURCE.updateDB(
+				MessageFormat.format(sql, updateQuery, insertQuery),
+				values.toArray());
+		if (counts == 0) {
+			throw new ToolException("一行も追加、更新されませんでした");
+		}
+	}
+
+	private String insertQuery(Map<String, String> args) {
+
+		StringBuilder cols = new StringBuilder();
+		StringBuilder places = new StringBuilder();
+		String splitter = "";
+		for (Map.Entry<String, String> arg : args.entrySet()) {
+			cols.append(splitter);
+			places.append(splitter);
+			splitter = ",";
+			cols.append(arg.getKey());
+			places.append("?");
+		}
+
+		// インサート時 IDは自動採番にする
+		cols.append(",ID ) values (");
+		cols.append(places.toString());
+		cols.append(" ,(select nvl((select Max(id) from noncompare_m),0) from dual) + 1)");
+
+		return "(" + cols.toString();
+
+	}
+}
