378103d29ed0b99bd41ac8ebb74efefb37035b04
daisuke-3 <d.kohgami@mosa-architect.com>
2012-09-25 17:34:04 +0900

    recommit

--- script/bin/regist.sh
+++ script/bin/regist.sh
@@ -20,7 +20,7 @@
 #    ログ登録のラッパーfunciton.
 #    パラメータのログ種別コードに応じて、各処理をcallする.
 #----------------------------------------------
-func_regist_log() {
+regist_log_data() {
 
 	MODE=$1
 	TODAY=`date +"%Y%m%d%H%M%S"`
@@ -38,22 +38,39 @@ func_regist_log() {
 		"1")
 			# Clientログ登録
 			${BASEDIR}/bin/regist_client.sh
-
+			mv ${CLIENT_LOG_DIR}/* ${CLIENT_LOG_DIR_REGIST}/
 			;;
 
 		"2")
 			# WEBServerログ登録
 			${BASEDIR}/bin/regist_web.sh
+			mv ${WEB_LOG_DIR}/* ${WEB_LOG_DIR_REGIST}/
 			;;
 
 		"3" | "4" | "5" | "6")
 			# 登録 OTX-CSS,OTX-ONL,AP-INFO,AP-HOST
 			${BASEDIR}/bin/regist_otx.sh $MODE
+
+			if [ $MODE == "3" ];then
+				mv ${OTXCSS_LOG_DIR}/* ${OTXCSS_LOG_DIR_REGIST}/
+			elif [ $MODE == "4" ];then
+				mv ${OTXONL_LOG_DIR}/* ${OTXONL_LOG_DIR_REGIST}/
+			elif [ $MODE == "5" ];then
+				mv ${APINFO_LOG_DIR}/* ${APINFO_LOG_DIR_REGIST}/
+			elif [ $MODE == "6" ];then
+				mv ${APHOST_LOG_DIR}/* ${APHOST_LOG_DIR_REGIST}/
+			fi
 			;;
 
 		"7")
 			# 登録 Tracelog
 			${BASEDIR}/bin/regist_trace.sh
+			mv ${OTXCSS_LOG_DIR}/* ${OTXCSS_LOG_DIR_REGIST}/
+			;;
+
+		"8")
+			# 登録 DBIOログ
+			${BASEDIR}/bin/regist_dbio.sh
 			;;
 
 		"9" | "10")
--- script/bin/test_regist_compare.sh
+++ script/bin/test_regist_compare.sh
@@ -2,7 +2,7 @@
 
 . $(dirname $0)/../bin/regist_compare.sh
 
-regist_compare 6 key6 item6 6
+regist_compare 0103 table1 3 2
 #regist_compare 5 5 key5 item5 5
 #regist_compare 1 a key1 item1 1
 #regist_compare 1
