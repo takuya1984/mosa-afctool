88a606d1ccec92bf6f56bb2a2aaecbda48e879f4
daisuke-3 <d.kohgami@mosa-architect.com>
2012-09-28 15:59:34 +0900

    fixed #21

--- script/bin/setup.sh
+++ script/bin/setup.sh
@@ -6,6 +6,15 @@ logdirs=(log log_regist log_target log_target_utf8)
 dirs=(01_client	02_web 03_otx-css 04_otx-onl 05_apinfo 06_aphost 07_trace 08_dbio 09_otx-sql 10_ap-sql)
 
 cd $basedir
+
+# 各種ディレクトリ作成
+maindirs=(bin conf jar tables tmp)
+for maindir in ${maindirs[@]}
+do
+	mkdir -p ${basedir}/${maindir}
+done
+
+# ログ用ディレクトリ作成
 for logdir in ${logdirs[@]}
 do
 	for dir in ${dirs[@]}
