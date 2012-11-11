c5e9c3b47e649599b5d97658955dd755c8a63a01
tyoshida <t.yoshida@mosa-architect.com>
2012-11-11 18:31:33 +0900

    fixed #52

--- norinToolTest/src/EntryPointTest.java
+++ norinToolTest/src/EntryPointTest.java
@@ -43,16 +43,43 @@ public class EntryPointTest {
 				.main(new String[] { "RegistCompareData_d", "ID=4", "MASTER_ID=3", "KEY=TESTKEY3", "ITEM_NAME=TESTITEM3", "UP_DOWN_CD=1"});
 
 	}
-	@Test
+//	@Test
 	public void Compare1() {
+//		EntryPoint
+//		.main(new String[] { "CompareData", "265", "272"});
 		EntryPoint
-				.main(new String[] { "CompareData", "265", "272"});
+		.main(new String[] { "CompareData", "1", "139"});
 
 	}
 //	@Test
 	public void Compare2_JYOGAI() {
 		EntryPoint
 				.main(new String[] { "CompareData_d", "1", "190"});
+	}
+	
+	@Test
+	public void Compare2() {
+//		EntryPoint
+//				.main(new String[] { "CompareData", "12", "2012-04-09-135759_00124_2_3104019_1.dat", "2012-08-02-191154501_01089_2_1108019_1.dat"});
+//
+//		EntryPoint
+//				.main(new String[] { "CompareData", "53", "2012-08-17-143122_55128_2_2302010_1.dat", "2012-04-06-142856007_00099_2_2302010_1.dat"});
+//		
+//		EntryPoint
+//				.main(new String[] { "CompareData", "46", "2012-08-17-143122_55128_2_2302010_1.dat", "2012-08-17-143122_55128_2_2302010_2.dat"});
+//		
+		EntryPoint
+				.main(new String[] { "CompareData", "78", "130529.421_ICC09_OJKF0.log", "20120817162128_1_IDB73_ODEN0.dat"});
+//
+//		EntryPoint
+//				.main(new String[] { "CompareData", "78", "130528.357_ICC09_S_T_N.log", "20120817162128_1_IDB73_ODEN0.dat"});
+//		
+//		EntryPoint
+//				.main(new String[] { "CompareData", "910", "2012-04-06-142846_00099_1_0801030_sql.dat", "2012-04-06-142846_00099_1_0801030_sql.dat"});
+//		
+//		EntryPoint
+//				.main(new String[] { "CompareData", "109", "2012-04-06-142847_00099_1_0801030_sql.dat", "2012-04-06-142846_00099_1_0801030_sql.dat"});
+
 	}
 
 }
